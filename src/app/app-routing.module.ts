68d21d5286755aca733e21505f53f59a47b872f8
josemouraocpt <josemouraocpt@gmail.com>
2022-09-15 00:32:18 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroClientesComponent } from './views/clientes/cadastro-clientes/cadastro-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cadastro-cliente', component: CadastroClientesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/clientes/cadastro-clientes/cadastro-clientes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-clientes',
+  templateUrl: './cadastro-clientes.component.html',
+  styleUrls: ['./cadastro-clientes.component.css']
+})
+export class CadastroClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  salvar(){
+    console.log('Cliente inserido com sucesso!');
+  }
+
+}
